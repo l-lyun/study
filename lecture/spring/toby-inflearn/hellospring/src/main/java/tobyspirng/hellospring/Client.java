6b2aef41942bb10af1daa03e6f6d42697d99e8f0
l-lyun <kimdohyun032@gmail.com>
2026-04-30 13:51:05 +0900

    section-3-5 오브젝트 팩토리 및 전략패턴

--- lecture/spring/toby-inflearn/hellospring/src/main/java/tobyspirng/hellospring/Client.java
+++ lecture/spring/toby-inflearn/hellospring/src/main/java/tobyspirng/hellospring/Client.java
@@ -6,8 +6,9 @@ import java.math.BigDecimal;
 public class Client {
 
 	public static void main(String[] args) throws IOException {
-		PaymentService paymentService = new PaymentService(new WebApiExRateProvider()); // 관계 설정의 책임을 클라이언트가 가져갔다
-		// PaymentService paymentService = new PaymentService(new SimpleExRateProvider());
+		ObjectFactory objectFactory = new ObjectFactory();
+		PaymentService paymentService = objectFactory.paymentService();
+
 		Payment payment = paymentService.prepare(100L, "USD", BigDecimal.valueOf(50.7));
 		System.out.println(payment);
 	}
