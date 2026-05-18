f0774c8d7b8c98de48b70fbbbe33542ffbb38f88
l-lyun <kimdohyun032@gmail.com>
2026-05-18 17:04:57 +0900

    section-4: 도메인 오브젝트 테스트

--- lecture/spring/toby-inflearn/hellospring/src/main/java/tobyspirng/hellospring/payment/PaymentService.java
+++ lecture/spring/toby-inflearn/hellospring/src/main/java/tobyspirng/hellospring/payment/PaymentService.java
@@ -16,13 +16,9 @@ public class PaymentService {
 	}
 
 	public Payment prepare(Long orderId, String currency, BigDecimal foreignCurrencyAmount) throws IOException {
-
-		// 환율을 가져오는 정책이나 방식이 변경되어도 외부에서 변경 가능
 		BigDecimal exRate = exRateProvider.getExRate(currency);
-		BigDecimal convertedAmount = foreignCurrencyAmount.multiply(exRate);
-		LocalDateTime validUntil = LocalDateTime.now(clock).plusMinutes(30);
 
-		return new Payment(orderId, currency,	foreignCurrencyAmount, exRate, convertedAmount, validUntil);
+		return Payment.createPrepared(orderId, currency,	foreignCurrencyAmount, exRate, LocalDateTime.now(clock));
 	}
 
 }
