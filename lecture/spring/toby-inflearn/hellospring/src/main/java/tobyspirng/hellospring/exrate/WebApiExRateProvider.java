409d9d87f635d67fde95150bf524aacb197479a0
l-lyun <kimdohyun032@gmail.com>
2026-05-20 17:28:43 +0900

    section-5: ExRateExecutor 콜백

--- lecture/spring/toby-inflearn/hellospring/src/main/java/tobyspirng/hellospring/exrate/WebApiExRateProvider.java
+++ lecture/spring/toby-inflearn/hellospring/src/main/java/tobyspirng/hellospring/exrate/WebApiExRateProvider.java
@@ -12,6 +12,8 @@ import java.util.stream.Collectors;
 import org.springframework.boot.json.JsonParseException;
 
 import tobyspirng.hellospring.api.ApiExecutor;
+import tobyspirng.hellospring.api.ErApiExRateExtractor;
+import tobyspirng.hellospring.api.ExRateExtractor;
 import tobyspirng.hellospring.api.SimpleApiExecutor;
 import tobyspirng.hellospring.payment.ExRateProvider;
 import tools.jackson.databind.ObjectMapper;
@@ -26,11 +28,11 @@ public class WebApiExRateProvider implements ExRateProvider {
 		String url = "https://open.er-api.com/v6/latest/" + currency;
 		// 콜백: new SimpleApiExecutor()
 		// 변하는 속성을 가진 코드는 콜백으로 메서드 파라미터 형태로 전달
-		return runApiForExRate(url, new SimpleApiExecutor());
+		return runApiForExRate(url, new SimpleApiExecutor(), new ErApiExRateExtractor());
 	}
 
 	// 템플릿: 내부 기능 실행
-	private static BigDecimal runApiForExRate(String url, ApiExecutor apiExecutor) {
+	private static BigDecimal runApiForExRate(String url, ApiExecutor apiExecutor, ExRateExtractor exRateExtractor) {
 		URI uri;
 		try {
 			uri = new URI(url);
@@ -46,16 +48,9 @@ public class WebApiExRateProvider implements ExRateProvider {
 			}
 
 		try {
-			return extractExRate(response);
+			return exRateExtractor.extractExRate(response);
 		} catch (JsonParseException e) {
 			throw new RuntimeException(e);
 		}
 	}
-
-	private static BigDecimal extractExRate(String response) {
-		ObjectMapper mapper = new ObjectMapper();
-		ExRateData data = mapper.readValue(response, ExRateData.class);
-		return data.rates().get("KRW");
-	}
-
 }
