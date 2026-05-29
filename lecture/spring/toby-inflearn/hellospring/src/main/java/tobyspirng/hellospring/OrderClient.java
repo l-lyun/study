308bd2bfe221910639d2b543b3b3a9fb933128fb
김도현 <142032659+l-lyun@users.noreply.github.com>
2026-05-29 17:26:49 +0900

    [Spring] 토비의 스프링 section 7 - 서비스 추상화 (#6)
    * section-7: 애플리케이션 서비스 도입
    
    * section-7: OrderRepository DIP
    
    * section-7: 기술에 독립적인 애플리케이션 서비스
    
    * section-7: OrderRepository DIP
    
    * section-7: 트랜잭션 서비스 추상화
    
    * section-7: JDBC 데이터 액세스 기술
    
    * section-7: 트랜잭션 프록시
    
    * section-7: 트랜잭션 프록시-2
    
    * section-7: @Transactional과 AOP

---
+++ lecture/spring/toby-inflearn/hellospring/src/main/java/tobyspirng/hellospring/OrderClient.java
@@ -0,0 +1,26 @@
+package tobyspirng.hellospring;
+
+import java.math.BigDecimal;
+
+import org.springframework.beans.factory.BeanFactory;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+import tobyspirng.hellospring.order.Order;
+import tobyspirng.hellospring.order.OrderService;
+import tobyspirng.hellospring.order.OrderServiceImpl;
+
+public class OrderClient {
+
+	public static void main(String[] args) {
+		BeanFactory beanFactory = new AnnotationConfigApplicationContext(OrderConfig.class);
+		OrderService service = beanFactory.getBean(OrderService.class);
+
+		Order order = service.createOrder(
+			"0100",
+			BigDecimal.TEN
+		);
+		System.out.println(order);
+
+	}
+
+}
