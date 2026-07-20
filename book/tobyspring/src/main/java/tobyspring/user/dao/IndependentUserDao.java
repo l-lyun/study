412c24c5eeae74c7a032b1f1bd6773ddef278014
김도현 <142032659+l-lyun@users.noreply.github.com>
2026-07-20 15:09:02 +0900

    [Spring] 토비의 스프링 Vol. 1 2장 테스트 (#9)
    * 2.2.1: 테스트 검증의 자동화
    
    * 2.2.2: 테스트의 효율적인 수행과 관리
    
    * 2.3.2: 테스트 결과의 일관성
    
    * 2.3.4: 테스트가 이끄는 개발
    
    * 2.4.1: @BeforeEach 활용 개선
    
    * 2.4.1: 테스트를 위한 애플리케이션 컨텍스트 관리
    
    * 2.5.2: 학습 테스트 - JUnit
    
    * 2.5.2: 학습 테스트 - ApplicationContext

--- book/tobyspring/src/main/java/tobyspring/user/dao/IndependentUserDao.java
+++ book/tobyspring/src/main/java/tobyspring/user/dao/IndependentUserDao.java
@@ -6,6 +6,8 @@ import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 
+import org.springframework.dao.EmptyResultDataAccessException;
+
 import tobyspring.user.domain.User;
 
 public class IndependentUserDao {
@@ -38,16 +40,43 @@ public class IndependentUserDao {
 		ps.setString(1, id);
 
 		ResultSet rs = ps.executeQuery();
-		rs.next();
-		User user = new User();
-		user.setId(rs.getString("id"));
-		user.setName(rs.getString("name"));
-		user.setPassword(rs.getString("password"));
+		User user = null;
+
+		if(rs.next()) {
+			user = new User();
+			user.setId(rs.getString("id"));
+			user.setName(rs.getString("name"));
+			user.setPassword(rs.getString("password"));
+		}
 
 		rs.close();
 		ps.close();
 		c.close();
 
+		if (user == null)
+			throw new EmptyResultDataAccessException(1);
+
 		return user;
+	}
+
+	public void deleteAll() throws SQLException, ClassNotFoundException {
+		Connection c = connectionMaker.makeNewConnection();
+		PreparedStatement ps = c.prepareStatement ("delete from users");
+		ps.executeUpdate();
+		ps.close();
+		c.close();
+	}
+
+	public int getCount() throws ClassNotFoundException, SQLException {
+		Connection c = connectionMaker.makeNewConnection();
+		PreparedStatement ps = c.prepareStatement("select count(*) from users");
+		ResultSet rs = ps.executeQuery();
+		rs.next();
+		int count = rs.getInt(1);
+		rs.close();
+		ps.close();
+		c.close();
+
+		return count;
 	}
 }
---
+++ book/tobyspring/src/test/java/tobyspring/user/UserDaoTest.java
@@ -0,0 +1,91 @@
+package tobyspring.user;
+
+import static org.assertj.core.api.Assertions.*;
+
+import java.sql.SQLException;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.dao.EmptyResultDataAccessException;
+import org.springframework.test.context.junit.jupiter.SpringJUnitConfig;
+
+import tobyspring.user.dao.DaoFactory;
+import tobyspring.user.dao.IndependentUserDao;
+import tobyspring.user.dao.UserDao;
+import tobyspring.user.domain.User;
+
+
+@SpringJUnitConfig(DaoFactory.class)
+public class UserDaoTest {
+
+	@Autowired
+	private IndependentUserDao userDao;
+
+	private User user1;
+	private User user2;
+	private User user3;
+
+	@BeforeEach
+	public void setup() {
+		this.user1 = new User("aa", "김김", "spring1");
+		this.user2 = new User("bb", "이이", "spring2");
+		this.user3 = new User("cc", "박박", "spring3");
+	}
+
+	@Test
+	public void addAndGet() throws SQLException, ClassNotFoundException {
+
+		userDao.deleteAll();
+		assertThat(userDao.getCount()).isEqualTo(0);
+
+		userDao.add(user1);
+		userDao.add(user2);
+
+		assertThat(userDao.getCount()).isEqualTo(2);
+
+		// id를 활용해 user를 가져오는 테스트 보강
+		User userGet1 = userDao.get(user1.getId());
+		assertThat(userGet1.getId()).isEqualTo(user1.getId());
+		assertThat(userGet1.getName()).isEqualTo(user1.getName());
+
+		User userGet2 = userDao.get(user2.getId());
+		assertThat(userGet2.getId()).isEqualTo(user2.getId());
+		assertThat(userGet2.getName()).isEqualTo(user2.getName());
+
+		// assertThat(userDao.getCount()).isEqualTo(1);
+		// User user2 = userDao.get(user.getId());
+		//
+		// assertThat(user.getId()).isEqualTo(user2.getId());
+		// assertThat(user.getName()).isEqualTo(user2.getName());
+		// assertThat(user.getPassword()).isEqualTo(user2.getPassword());
+	}
+
+	@Test
+	public void count() throws SQLException, ClassNotFoundException {
+
+		userDao.deleteAll();
+		assertThat(userDao.getCount()).isEqualTo(0);
+
+		userDao.add(user1);
+		assertThat(userDao.getCount()).isEqualTo(1);
+
+		userDao.add(user2);
+		assertThat(userDao.getCount()).isEqualTo(2);
+
+		userDao.add(user3);
+		assertThat(userDao.getCount()).isEqualTo(3);
+	}
+
+	@Test
+	public void getUserFailure() throws SQLException, ClassNotFoundException{
+
+		userDao.deleteAll();
+		assertThat(userDao.getCount()).isEqualTo(0);
+
+		assertThatThrownBy(() -> userDao.get("unknown"))
+			.isInstanceOf(EmptyResultDataAccessException.class);
+	}
+}
