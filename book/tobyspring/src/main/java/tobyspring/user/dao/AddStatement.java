20a99e71aa322a0cfda58b43267d77f3ffedf71c
unknown <kimdohyun032@gmail.com>
2026-07-21 16:19:41 +0900

    3.3.1: 전략 클래스의 추가 정보

---
+++ book/tobyspring/src/main/java/tobyspring/user/dao/AddStatement.java
@@ -0,0 +1,32 @@
+package tobyspring.user.dao;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+import tobyspring.user.domain.User;
+
+
+public class AddStatement implements StatementStrategy {
+
+  private final User user;
+
+  public AddStatement(User user) {
+    this.user = user;
+  }
+
+
+  @Override
+  public PreparedStatement makePreparedStatement(Connection c) throws SQLException {
+    
+		PreparedStatement ps = c.prepareStatement(
+			"insert into users(id, name, password) values (?, ?, ?)"
+		);
+		ps.setString(1, user.getId());
+		ps.setString(2, user.getName());
+		ps.setString(3, user.getPassword());
+
+		return ps;
+	}
+  
+  
+}
